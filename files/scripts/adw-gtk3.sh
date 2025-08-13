84762cdc1e8b272acf1a49f861675bac52f4af64
spinnedrift <treecomputes+git@proton.me>
2025-08-12 20:40:19 -0700

    Update

---
+++ files/scripts/adw-gtk3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://github.com/lassekongo83/adw-gtk3/releases/download/v6.2/adw-gtk3v6.2.tar.gz
+tar -xvf ./adw-gtk3v6.2.tar.gz
+cp -rfv ./adw-gtk3v6.2/themes/* /usr/share/themes
+rm -rfv ./adw-gtk3v6.2.tar.gz
