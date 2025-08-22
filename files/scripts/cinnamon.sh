0072f22f8a8a1aec861a8788f30d37c1ee649594
spinnedrift <treecomputes+git@proton.me>
2025-08-21 21:24:58 -0700

    Quick hotfix

--- files/scripts/cinnamon.sh
+++ files/scripts/cinnamon.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo dnf install @cinnamon-desktop-environment kde-partitionmanager --exclude abrt*,gnome-terminal,gnome-software,gnome-software-rpm-ostree,gnome-software-fedora-langpacks,gnome-system-monitor,gnome-calculator,mpv,pidgin,hexchat,transmission,eom,simple-scan,thunderbird,PackageKit*
+sudo dnf install -y @cinnamon-desktop-environment kde-partitionmanager --exclude abrt*,gnome-terminal,gnome-software,gnome-software-rpm-ostree,gnome-software-fedora-langpacks,gnome-system-monitor,gnome-calculator,mpv,pidgin,hexchat,transmission,eom,simple-scan,thunderbird,PackageKit*
